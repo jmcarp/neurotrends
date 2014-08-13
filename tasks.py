5ff062f7d1552cddf756f5eb47c5e16475a05255
Joshua Carp <jm.carp@gmail.com>
2014-08-12 21:36:52 -0400

    Add backup and restore tasks; gitignore default backup directory.

--- tasks.py
+++ tasks.py
@@ -7,6 +7,8 @@ DB_PATH = '/usr/local/var/mongodb'
 LOG_PATH = '/usr/local/var/log/mongodb/mongo.log'
 
 
+# TODO: Add port, database name to MongoDB tasks
+
 @task
 def mongod(db_path=DB_PATH, fork=False, log_path=LOG_PATH):
     cmd = 'mongod --dbpath {0}'.format(db_path)
@@ -14,6 +16,24 @@ def mongod(db_path=DB_PATH, fork=False, log_path=LOG_PATH):
         cmd += ' --fork'
     if log_path:
         cmd += ' --logpath {0}'.format(log_path)
+    run(cmd)
+
+
+@task
+def mongodump(out_path=None):
+    cmd = 'mongodump --db neurotrends'
+    if out_path:
+        cmd += ' --out {0}'.format(out_path)
+    run(cmd)
+
+
+@task
+def mongorestore(db_path=DB_PATH, path=None, drop=False):
+    cmd = 'mongorestore --db neurotrends'
+    if drop:
+        cmd += ' --drop'
+    if path:
+        cmd += path
     run(cmd)
 
 
