69b860455bc9c178b2e639ae0e7df63a9d43f581
Joshua Carp <jm.carp@gmail.com>
2014-08-16 23:44:04 -0400

    Specify user agent.

--- neurotrends/model/config.py
+++ neurotrends/model/config.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+
 import os
 
 from sciscrape.scrapetools.scrape import Scrape, UMScrape
 from sciscrape.scrapetools.scrape import EXTENSIONS
+
 
 SCRAPE_CLASS = UMScrape
 
@@ -9,6 +12,9 @@ USER_FILE = '/Users/jmcarp/private/ump'
 SCRAPE_KWARGS = {
     'timeout': 30,
     'user_file': USER_FILE,
+    'agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) '
+             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 '
+             'Safari/537.36',
 }
 DOCUMENT_TYPES = ['pmc', 'html', 'pdf']
 VERIFY_THRESHOLD = 0.9
@@ -33,3 +39,4 @@ OPENURL_URL = 'http://www.crossref.org/openurl/'
 EMAIL_ADDR = 'jm.carp@gmail.com'
 
 PDF_MIN_LENGTH = 1000
+
