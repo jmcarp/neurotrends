131c3a7e611a8c8ef3efc9d1db9c9439d334ac84
jmcarp <jm.carp@gmail.com>
2014-10-07 23:19:26 -0400

    Add about view.

--- neuroapp/app/scripts/controllers/about.js
+++ neuroapp/app/scripts/controllers/about.js
@@ -9,9 +9,6 @@
  */
 angular.module('neuroApp')
   .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.address = 'jm.carp@gmail.com';
   });
+
